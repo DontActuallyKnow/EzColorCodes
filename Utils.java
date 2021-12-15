a837c11f2a2c1d2a7d1a490e81b4d819f5318bd7
DontActuallyKnow <rockylee3@outlook.com>
2021-12-15 20:15:52 +0000

    Class name change
    EzColor --> Utils
    Also fixed functionality in the code, was not working beforehand

---
+++ Utils.java
@@ -0,0 +1,27 @@
+package me.DontActuallyKnow.SkyblockUltra;
+
+import org.apache.commons.lang3.Validate;
+
+public class Utils {
+
+    public static final char COLOUR_CHAR = '\u00A7';
+
+    public static String translateAlternativeColourCode(char alternateColourCode, String string){
+        Validate.notNull(string, "Cannot translate null text");
+
+        char[] b = string.toCharArray();
+        for (int i = 0; i < b.length - 1; i++){
+            if(b[i] == alternateColourCode && "0123456789AaBbCcDdEeFfKkLlMmNnOoRrXx".indexOf(b[i + 1]) > -1){
+                b[i] = Utils.COLOUR_CHAR;
+                b[i + 1] = Character.toLowerCase(b[i + 1]);
+
+            }
+        }
+        return  new String(b);
+    }
+
+    public static String chat(String string){
+        return Utils.translateAlternativeColourCode('&', string);
+    }
+
+}
